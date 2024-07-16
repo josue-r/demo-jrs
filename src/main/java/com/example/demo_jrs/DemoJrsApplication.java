08f3c7e5e60443a599b3c9ab8dff341ed6b51bd5
Josue-RS <josue.ruiz@wizeline.com>
2024-07-16 16:15:10 -0600

    initial commit

---
+++ src/main/java/com/example/demo_jrs/DemoJrsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_jrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_jrs/DemoJrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_jrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
